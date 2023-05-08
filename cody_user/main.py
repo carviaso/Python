ef35396a9caae77f6d9511132f823ea6fd6f5641
carviaso@gmail.com <Carlos>
2023-05-07 23:06:00 -0600

    ok

---
+++ cody_user/main.py
@@ -0,0 +1,14 @@
+# PEP-8
+
+
+#CamelCase
+class User():
+    def __init__(self, username, password=''):
+        self.username = username
+        self.password = password
+
+    def set_password(self):
+        pass
+
+# no menclatura snake_case
+cody_user = User('Cody')
